edd10ba02e9cb84c14515fe98b57442bdf417a42
KS, Ranjith <Ranjith.KS@ncr.com>
2019-08-25 15:47:26 +0530

    print command examples

---
+++ basics/print.py
@@ -0,0 +1,23 @@
+print("Hi Ranjith")
+
+print("[\"]Hi Ran\tjith\n")
+print('[\']Hi Ran\tjith\n')
+
+print ("Hi", end = "") # char at end can be anything; no spaces
+print ("Ranjith")
+
+print ("Hi", end = " ") # char at end can be anything; 1 space
+print ("Ranjith")
+
+print ("Hi", end = ";") # char at end can be anything; semi colon
+print ("Ranjith")
+
+print ("Hi", end = "\n") # char at end can be anything; new line
+print ("Ranjith")
+
+print ("Hi", end = "\t") # char at end can be anything; tab
+print ("Ranjith")
+
+print ("Hi", end = "!@#$") # char at end can be anything; some random string
+print ("Ranjith")
+
