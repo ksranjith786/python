3af43b9b349e3d21054ba5fac3e404c40a399281
KS, Ranjith <Ranjith.KS@ncr.com>
2019-08-25 16:05:37 +0530

    variables and data types

---
+++ basics/variables.py
@@ -0,0 +1,27 @@
+i = 10
+f = 3.14
+d = 1.620754
+c = 'c'
+s = "String"
+
+print (i)
+print (f)
+print (d)
+print (c)
+print (s)
+
+print (i, f, d, c, s)
+
+print (type(i))
+print (type(f))
+print (type(d))
+print (type(c))
+print (type(s))
+
+print (type(1))
+print (type(3.14))
+
+
+print (type('no format'))
+print (type(b'byte format'))
+print (type(u'unicode format'))
