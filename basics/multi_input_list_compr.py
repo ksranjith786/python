fbe3f473a2328c2f63b2f872e3f7ae614d45b489
KS, Ranjith <Ranjith.KS@ncr.com>
2019-09-06 10:11:50 +0530

    more modifications

--- basics/multi_input_list_compr.py
+++ basics/multi_input_list_compr.py
@@ -25,3 +25,21 @@ print()
 # taking multiple inputs at a time  
 x = [int(x) for x in input("Enter multiple int values: ").split()] 
 print("Number of list is: ", x)
+
+'''
+$ python multi_input_list_compr.py
+Enter two int values (separated by space): 12 3
+First Number is:  12
+Second Number is:  3
+
+Enter three float values (separated by ;): 1.234;45.32;12
+First float Number is:  1.234
+Second float Number is:  45.32
+Third float Number is:  12.0
+
+Enter only two strings (separated by $): A4t$%^&
+First string is A4t and second string is %^&
+
+Enter multiple int values: 66 77 3 244 21 7 29
+Number of list is:  [66, 77, 3, 244, 21, 7, 29]
+'''
